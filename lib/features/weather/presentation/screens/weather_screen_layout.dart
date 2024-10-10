72c5917120ad459596f71577c3d8771719fb4a15
SelloN <102992967+Sello23@users.noreply.github.com>
2024-10-10 23:51:04 +0200

    Wrap up UI

--- lib/features/weather/presentation/screens/weather_screen_layout.dart
+++ lib/features/weather/presentation/screens/weather_screen_layout.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app_flutter/features/weather/data/models/weather_data.dart';
+import 'package:weather_app_flutter/shared/widgets/thumb_nail_image.dart';
+
+import '../../data/models/weather_forecast.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreenLayout extends StatelessWidget {
   const WeatherScreenLayout({
@@ -11,6 +15,8 @@ class WeatherScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WeatherForecast weatherForecast = weatherData!.list[0];
+
     return Scaffold(
       backgroundColor: Colors.lightBlue,
       body: Center(
@@ -23,205 +29,121 @@ class WeatherScreenLayout extends StatelessWidget {
                   alignment: Alignment.center,
                   child: Image.asset(
                     'assets/images/clear_background.png',
-                    height: 450,
+                    height: 420,
                     width: double.infinity,
                     fit: BoxFit.fill,
                   ),
                 ),
-                Positioned(
-                  top: 150,
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: const Column(
+                _displayTodayWeather(weatherForecast),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          "20°",
-                          style: TextStyle(
+                        SizedBox(
+                          width: 90,
+                          child: Text(
+                            "${weatherForecast.temp.min.round()}°\nmin",
+                            style: const TextStyle(
+                              fontSize: 17,
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 70.0),
+                            ),
+                          ),
                         ),
-                        Text(
-                          "SUNNY",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 40.0),
+                        SizedBox(
+                          width: 60,
+                          child: Text(
+                            "${weatherForecast.temp.day.round()}°\ncurrent",
+                            style: const TextStyle(fontSize: 17, color: Colors.white),
+                          ),
                         ),
+                        SizedBox(
+                          width: 37,
+                          child: Text(
+                            "${weatherForecast.temp.max.round()}°\nmax",
+                            style: const TextStyle(
+                                fontSize: 17, color: Colors.white),
+                          ),
+                        )
                       ],
+                    ),
+                  ),
+                ),
+                const Divider(
+                  thickness: 2,
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Expanded(
+                    child: Column(
+                      children: weatherData!.list
+                          .map((item) => _displayWeatherDetailRow(item))
+                          .toList(),
                     ),
                   ),
                 ),
               ],
             ),
-            const Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(17.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        width: 90,
-                        child: Text(
-                          "20°\nmin",
-                          style: TextStyle(fontSize: 17, color: Colors.white, ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 60,
-                        child: Text(
-                          "20°\ncurrent",
-                          style: TextStyle(fontSize: 17, color: Colors.white),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 37,
-                        child: Text(
-                          "20°\nmax",
-                          style: TextStyle(fontSize: 17, color: Colors.white),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Divider(
-                  thickness: 2,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _displayWeatherDetailRow(WeatherForecast weatherItem) {
+    final imageUrl =
+        "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png";
+    var date = DateTime.fromMillisecondsSinceEpoch(weatherItem.dt * 1000);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(
+          width: 90,
+          child: Text(
+            DateFormat.EEEE().format(date),
+            style: const TextStyle(fontSize: 17, color: Colors.white),
+          ),
+        ),
+        ThumbNailImage(thumbnailUri: imageUrl),
+        SizedBox(
+          width: 30,
+          child: Text(
+            "${weatherItem.temp.day.round()}°",
+            style: const TextStyle(fontSize: 17, color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Positioned _displayTodayWeather(WeatherForecast weatherForecast) {
+    return Positioned(
+      top: 150,
+      child: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            Text(
+              "${weatherForecast.temp.day.round()}°",
+              style: const TextStyle(
                   color: Colors.white,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(17.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 90,
-                            child: Text(
-                              "Tuesday",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          ),
-                          Icon(
-                            Icons.ac_unit_rounded,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 30,
-                            child: Text(
-                              "20°",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 90,
-                            child: Text(
-                              "Wednesday",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          ),
-                          Icon(
-                            Icons.ac_unit_rounded,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 30,
-                            child: Text(
-                              "20°",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 90,
-                            child: Text(
-                              "Thursday",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          ),
-                          Icon(
-                            Icons.ac_unit_rounded,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 30,
-                            child: Text(
-                              "20°",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 90,
-                            child: Text(
-                              "Friday",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          ),
-                          Icon(
-                            Icons.ac_unit_rounded,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 30,
-                            child: Text(
-                              "20°",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 90,
-                            child: Text(
-                              "Saturday",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          ),
-                          Icon(
-                            Icons.ac_unit_rounded,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 30,
-                            child: Text(
-                              "20°",
-                              style:
-                                  TextStyle(fontSize: 17, color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 70.0),
+            ),
+            Text(
+              weatherForecast.weather[0].main.toUpperCase(),
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0),
             ),
           ],
         ),
